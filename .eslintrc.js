cd5f04ae7e81f06466354803dd2c06f4d2fe5c19
sistemaon <erni.augusto@gmail.com>
2018-02-18 23:46:56 -0300

    project configs
    editorconfig, eslint airbnb ok

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        // "semi": ["error", "always"],
+        "semi": "off",
+        "quotes": ["warn", "single"],
+        "no-console": "off"
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+let t;
+t = 0;
+t = 1;
+console.log('teste: ', t);
+// console.log("t: ", t);
